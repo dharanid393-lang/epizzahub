9ca4ea1508c9e1d72482fb9457927a3dbe5270f1
dharanid393-lang <dharanid393@gmail.com>
2026-05-08 22:53:43 +0530

    Add files via upload

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,38 @@
+﻿using ePizza.Application.Contracts;
+using ePizza.Application.DTOs.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ePizza.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TokenController : ControllerBase
+    {
+        private readonly ITokenGeneratorService _tokenGeneratorService;
+
+        public TokenController(ITokenGeneratorService tokenGeneratorService)
+        {
+            _tokenGeneratorService = tokenGeneratorService;
+        }
+
+        [HttpGet]
+        [Route("getToken/{userName}/{password}")]
+        public async Task<IActionResult> GetToken(string userName, string password)
+        {
+            var token = await _tokenGeneratorService.GenerateTokenAsync(userName, password);
+
+            return Ok(token);
+        }
+
+        [HttpPost]
+        [Route("token-refresh")]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequestDto requestDto)
+        {
+            var token = await _tokenGeneratorService.GenerateRefreshToken(requestDto);
+
+            return Ok(token);
+        }
+    }
+}
---
+++ Middleware/CommonResponseMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using ePizza.Application.DTOs.Common;
+using System.Text.Json;
+
+namespace ePizza.API.Middleware
+{
+    public class CommonResponseMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CommonResponseMiddleware(RequestDelegate next) 
+        {
+
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var originalBody =  context.Response.Body;
+
+            var traceId = Guid.NewGuid().ToString();
+
+            context.Items["TraceId"] = traceId;
+
+
+            using var memoryStream = new MemoryStream();
+            context.Response.Body = memoryStream;   
+
+            await _next(context);
+
+            if (context.Response.ContentType != null &&
+                context.Response.ContentType.Contains("application/json") && context.Request.Path.Value != "/health")
+            
+            
+            {
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                var responseBody = await new StreamReader(memoryStream).ReadToEndAsync();
+
+                var responseObj
+                     = new ApiResponseModelDto<object>(
+                         isSuccess: context.Response.StatusCode is >= 200 and <= 299,
+                         data: JsonSerializer.Deserialize<object>(responseBody)!,
+                         message: context.Response.StatusCode is >= 200 and <= 299 ? "Request Completed Successfully" : "Something went wrong");
+
+                var jsonResponse = JsonSerializer.Serialize(responseObj);
+
+                context.Response.Body = originalBody;
+
+                await context.Response.WriteAsync(jsonResponse);
+
+            }
+
+        }
+    }
+}
